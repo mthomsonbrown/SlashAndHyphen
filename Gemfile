b21a797fca321864ba4c6d3face0542703d75bc9
ookamijin <mthomsonbrown@gmail.com>
2015-10-02 01:47:05 +0000

    Converted to postgres

--- Gemfile
+++ Gemfile
@@ -18,7 +18,8 @@ gem 'active_model_serializers', '~> 0.8.3'
 gem 'rails-api'
 
 group :development, :test do
-  gem 'sqlite3',     '1.3.9'
+  gem 'pg',             '0.17.1'
+  #gem 'sqlite3',     '1.3.9'
   gem 'byebug',      '3.4.0'
   gem 'web-console', '2.0.0.beta3'
   gem 'spring',      '1.1.3'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151001180555) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "beers", force: :cascade do |t|
     t.string   "name"
     t.decimal  "abv"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20151001180555) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "beers", ["user_id"], name: "index_beers_on_user_id"
+  add_index "beers", ["user_id"], name: "index_beers_on_user_id", using: :btree
 
   create_table "flavors", force: :cascade do |t|
     t.string   "name"
@@ -32,8 +35,8 @@ ActiveRecord::Schema.define(version: 20151001180555) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "flavors", ["beer_id"], name: "index_flavors_on_beer_id"
-  add_index "flavors", ["user_id"], name: "index_flavors_on_user_id"
+  add_index "flavors", ["beer_id"], name: "index_flavors_on_beer_id", using: :btree
+  add_index "flavors", ["user_id"], name: "index_flavors_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -44,7 +47,7 @@ ActiveRecord::Schema.define(version: 20151001180555) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
